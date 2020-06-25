5278371055edee15edea6f9a4cc5dd9f48f12850
anant2307 <50878871+anant2307@users.noreply.github.com>
2020-06-25 08:18:52 +0530

    An Android app
    which helps owner to upload rooms and renter can search rooms

---
+++ app/src/main/java/com/abhishek/io/Quickrent/Availablity.java
@@ -0,0 +1,10 @@
+package com.abhishek.io.Quickrent;
+
+import com.abhishek.io.Quickrent.model.Rent;
+import com.abhishek.io.Quickrent.model.User;
+
+public class Availablity {
+
+    public static User currentUser;
+    public static Rent currentRent;
+}
